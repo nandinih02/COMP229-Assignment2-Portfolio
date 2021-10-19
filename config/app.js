38e8a6485d4e2b1bafb602971f96563c5526dfcc
Nandini <juicyfruit5600@yahoo.ca>
2021-10-19 01:26:41 -0400

    Added Update and Delete Buttons

--- config/app.js
+++ config/app.js
@@ -20,7 +20,6 @@ console.log('Connected to MongoDB...');
 });
 
 let indexRouter = require('../routes/index');
-let usersRouter = require('../routes/users');
 let contactsRouter = require('../routes/contacts');
 
 let app = express();
@@ -37,7 +36,6 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, '../node_modules')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/contacts-list', contactsRouter);
 
 // catch 404 and forward to error handler
