3b17b65c07d30b845c0e188a724b054594143fd6
Nandini <juicyfruit5600@yahoo.ca>
2021-10-23 17:15:02 -0400

    Updated db URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports =
 {
-    "URI": "mongodb://localhost/business_contacts"
+    //"URI": "mongodb://localhost/business_contacts"
+    "URI": "mongodb+srv://Nandini:zvkyHdcQYu1oheGX@mongodbserver.1pggn.mongodb.net/business_contacts?retryWrites=true&w=majority"
 }
